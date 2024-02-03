df5b8fa7c7e6da91eed9fd08ad9d90a26390ae47
sing3demons <sing3demons@live.com>
2024-02-03 10:12:32 +0700

    Add nanoid utility for generating random IDs

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ class Server {
       port = process.env.PORT ?? 3000,
       kafka = new KafkaNode()
 
+    app.use(express.urlencoded({ extended: true }))
     app.use(express.json({ limit: '50mb' }))
     app.use(helmet())
     app.use(setRequestID())
