24602c0e1fe54f486aaa3f5fbe8e4cbbe4aca406
sing3demons <sing3demons@live.com>
2024-02-03 11:29:50 +0700

    Refactor NanoIdService to allow customizing the ID length

--- src/lib/utils/nanoid.ts
+++ src/lib/utils/nanoid.ts
@@ -1,11 +1,18 @@
 import { customAlphabet } from 'nanoid'
 
+const ALPHABET: string = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
 export class NanoIdService {
-  private readonly alphanum = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  private readonly alphanum = ALPHABET
   private readonly len = 11
 
-  randomNanoId(): string {
-    const nanoid = customAlphabet(this.alphanum, this.len)
+  randomNanoId(size?: number): string {
+    const nanoid = customAlphabet(this.alphanum, size ?? this.len)
+    return nanoid()
+  }
+
+  static randomNanoId(): string {
+    const nanoid = customAlphabet(ALPHABET, 11)
     return nanoid()
   }
 
